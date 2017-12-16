97f16d4a2e28d57bf947c91a0460a219a4956cf7
lukasheinrich <lukas.heinrich@gmail.com>
2017-12-16 15:48:26 +0100

    blueprint

---
+++ yadagehttpctrl/yadageblueprint.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify, request
+from flask import current_app as app
+
+blueprint = Blueprint('yadage', __name__)
+
+@blueprint.route('/')
+def ui():
+    return app.send_static_file('ui.html')
+
+@blueprint.route('/ctrl/read/<method>')
+def readmethod(method):
+    return jsonify(getattr(app.config['yadage_controller'], method)())
+
+@blueprint.route('/ctrl/write/<method>', methods=['POST'])
+def writemethod(method):
+    getattr(app.config['yadage_controller'], method)(**request.json)
+    return jsonify({'status': 'ok'})
+
+@blueprint.route('/state')
+def state():
+    app.config['yadage_controller'].sync_backend()
+    return jsonify(app.config['yadage_controller'].adageobj.json())
+
+@blueprint.route('/state/rule/<ruleid>')
+def rule(ruleid):
+    return jsonify(app.config['yadage_controller'].adageobj.view().getRule(identifier = ruleid).json())
+
+@blueprint.route('/state/node/<nodeid>')
+def node(nodid):
+    return jsonify(app.config['yadage_controller'].adageobj.view().getNode(identifier = nodeid).json())
--- yadagehttpctrl/yadagehttpserver.py
+++ yadagehttpctrl/yadagehttpserver.py
@@ -2,40 +2,24 @@ import click
 from flask import Flask, jsonify, request
 
 import pkg_resources
-import yadage.utils
-import yadage.wflowstate
-import yadage.controllers
+
+
 
 static_path = pkg_resources.resource_filename('yadagehttpctrl', 'static')
 app = Flask('yadagectrl',static_folder=static_path)
 
-@app.route('/')
-def ui():
-    return app.send_static_file('ui.html')
 
-@app.route('/ctrl/read/<method>')
-def readmethod(method):
-    return jsonify(getattr(app.config['yadage_controller'], method)())
+def init_app(app, statetype, stateopts, backendstring):
+    from yadage.wflowstate import load_model_fromstring
+    from yadage.utils import setupbackend_fromstring
+    from yadage.controllers import PersistentController
+    model      = load_model_fromstring(statetype,stateopts)
+    backend    = setupbackend_fromstring(backendstring)
+    app.config['yadage_controller'] = PersistentController(model,backend)
 
+    from yadageblueprint import blueprint
 
-@app.route('/ctrl/write/<method>', methods=['POST'])
-def writemethod(method):
-    getattr(app.config['yadage_controller'], method)(**request.json)
-    return jsonify({'status': 'ok'})
-
-@app.route('/state')
-def state():
-    app.config['yadage_controller'].sync_backend()
-    return jsonify(app.config['yadage_controller'].adageobj.json())
-
-@app.route('/state/rule/<ruleid>')
-def rule(ruleid):
-    return jsonify(app.config['yadage_controller'].adageobj.view().getRule(identifier = ruleid).json())
-
-@app.route('/state/node/<nodeid>')
-def node(nodid):
-    return jsonify(app.config['yadage_controller'].adageobj.view().getNode(identifier = nodeid).json())
-
+    app.register_blueprint(blueprint)
 
 @click.command()
 @click.argument('statetype')
@@ -44,16 +28,12 @@ def node(nodid):
 @click.option('--ip', default = '0.0.0.0', help = 'host ip on which to server')
 @click.option('--port', default = 8000, help = 'port on which to server')
 def serve(statetype, backend, modelopt, ip ,port):
+    from yadage.utils import options_from_eqdelimstring
 
 
-    stateopts  = yadage.utils.options_from_eqdelimstring(modelopt)
-    stateopts  = yadage.utils.options_from_eqdelimstring(modelopt)
-    model      = yadage.wflowstate.load_model_fromstring(statetype,stateopts)
-    backend    = yadage.utils.setupbackend_fromstring(backend)
-    controller = yadage.controllers.PersistentController(model,backend)
+    stateopts  = options_from_eqdelimstring(modelopt)
+    init_app(app, statetype, stateopts, backend)
 
-
-    app.config['yadage_controller'] = controller
     app.run(host=ip, port=port)
 
 if __name__ == '__main__':
