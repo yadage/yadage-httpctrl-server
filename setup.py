cefda1b190c14363ecb8547389cc069172e81d9d
lukasheinrich <lukas.heinrich@gmail.com>
2018-03-24 16:45:41 +0100

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'yadage-httpctrl-server',
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'yadage http controller interfacint to persistent yadage controller',
   url = '',
   author = 'Lukas Heinrich',
